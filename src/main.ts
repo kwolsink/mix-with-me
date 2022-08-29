b7d7518d7b98489b664309c3085900e61bb79636
Kasper <kwolsink@gmail.com>
2022-08-29 22:14:29 +0200

    refactor router name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Router from './Router'
+import Router from './router'
 
 // Firebase
 import { initializeApp } from "firebase/app";
