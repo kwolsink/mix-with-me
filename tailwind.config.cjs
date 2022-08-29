3c2b59a855ed6318932f963df29944064f80e35b
Kasper <kwolsink@gmail.com>
2022-08-29 17:54:12 +0200

    add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)"
+      },
+    },
   },
   plugins: [],
 }
