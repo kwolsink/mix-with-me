dc97d9f7912abce6f0b3c624cf47f2ec7800c052
Kasper <kwolsink@gmail.com>
2022-08-29 18:01:09 +0200

    Compelete homepage styling

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="flex flex-col items-center h-full">
-        <h1 class="text-primary-color font-bold">Mix With Me</h1>
+    <div class="flex flex-col items-center h-full pt-5 pb-5">
+        <h1 class="text-primary-color font-bold text-4xl mt-20">Mix With Me</h1>
         <p class="m-8 text-center"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Dolores nemo enim, accusamus, quaerat recusandae dolor ut
              nulla sequi perspiciatis ipsam quo quasi obcaecati fugiat.
               Provident ea debitis quisquam maiores praesentium?
         </p>
-        <button class="p-2 m-3 w-10/12 text-white bg-primary-color rounded">Register</button>
-        <button class="p-2 m-3 w-10/12 text-secondary-color rounded border-secondary-color border-2">Already have an account?</button>
+        <div class="mt-auto mb-32">
+            <button class="p-2 m-3 w-10/12  text-white bg-primary-color rounded">Register</button>
+            <button class="p-2 m-3 w-10/12 text-secondary-color rounded border-secondary-color border-2">Already have an account?</button>
+        </div>
     </div>
 </template>
 
