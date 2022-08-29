ffdabbf321c6540c5e97a8f1b92c77a7d48f0645
Kasper <kwolsink@gmail.com>
2022-08-29 17:23:48 +0200

    basic welcome page skeleton

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+    import WelcomePage from './components/WelcomePage.vue';
+</script>
+
+<template>
+    <div>
+        <WelcomePage></WelcomePage>
+    </div>
+</template>
+
---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col items-center">
+        <h1>Mix With Me</h1>
+        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Dolores nemo enim, accusamus, quaerat recusandae dolor ut
+             nulla sequi perspiciatis ipsam quo quasi obcaecati fugiat.
+              Provident ea debitis quisquam maiores praesentium?
+        </p>
+        <button>Register</button>
+        <button>Already have an account?</button>
+    </div>
+</template>
+
