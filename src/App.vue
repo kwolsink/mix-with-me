9a2e3eec42e1410809924ed952f1a9d4eaf2be39
Kasper <kwolsink@gmail.com>
2022-08-29 17:56:29 +0200

    fix height setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
-    import WelcomePage from './components/WelcomePage.vue';
+import WelcomePage from './components/WelcomePage.vue';
 </script>
 
 <template>
-    <div>
+    <div id="view">
         <WelcomePage></WelcomePage>
     </div>
 </template>
 
+<style scoped>
+#view {
+    height: 100vh;
+    min-height: 100vh;
+}
+</style>
