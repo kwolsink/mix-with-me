a998ba39d9e1c81d5e5054bfa72dffb258c2d6ea
Kasper <kwolsink@gmail.com>
2022-08-29 17:54:39 +0200

    add some styling to the welcome page

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex flex-col items-center">
-        <h1>Mix With Me</h1>
-        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    <div class="flex flex-col items-center h-full">
+        <h1 class="text-primary-color font-bold">Mix With Me</h1>
+        <p class="m-8 text-center"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Dolores nemo enim, accusamus, quaerat recusandae dolor ut
              nulla sequi perspiciatis ipsam quo quasi obcaecati fugiat.
               Provident ea debitis quisquam maiores praesentium?
         </p>
-        <button>Register</button>
-        <button>Already have an account?</button>
+        <button class="p-2 m-3 mt-auto w-10/12 text-white bg-primary-color rounded">Register</button>
+        <button class="p-2 m-3 mt-auto w-10/12 text-secondary-color rounded border-secondary-color border-2">Already have an account?</button>
     </div>
 </template>
 
