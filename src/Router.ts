7e50cd045281dc30e1690361b41e2bbe84574466
Kasper <kwolsink@gmail.com>
2022-08-29 22:14:25 +0200

    Add configure route

--- src/Router.ts
+++ src/Router.ts
@@ -1,8 +1,11 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+
 import WelcomePage from './components/WelcomePage.vue'
+import ArtistConfigurationPage from './components/ArtistConfigurationPage.vue'
 
 const routes = [
-    { path: '/', component: WelcomePage} 
+    { path: '/', component: WelcomePage},
+    { path: '/configure', component: ArtistConfigurationPage}
 ]
 
 const router = createRouter({
