908ebee861cc1dd460d2172fa1c0f3972b727f99
Kasper <kwolsink@gmail.com>
2022-08-29 18:52:19 +0200

    user promise api to handle google auth

--- src/composables/authentication.ts
+++ src/composables/authentication.ts
@@ -1,14 +1,18 @@
-import {auth} from '../main'
-import {signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import { auth } from '../main'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 const googleProvider = new GoogleAuthProvider()
 
-export function authenticateWithGoogle() {
-    signInWithPopup(auth, googleProvider).then(result => {
+export async function authenticateWithGoogle() {
+    try {
+        const result = await signInWithPopup(auth, googleProvider)
         const credential = GoogleAuthProvider.credentialFromResult(result)
         const token = credential?.accessToken
         const user = result.user
-    }).catch(error => {
+        return true
+    }
+    catch {
         console.error('Something went wrong with google authentication.')
-    })
-}
+        return false
+    }
+}   
