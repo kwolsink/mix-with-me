7609e3637f3325450450eacb09b6147cec502870
Kasper <kwolsink@gmail.com>
2022-09-06 16:38:25 +0200

    Add carousel route and component

--- src/Router.ts
+++ src/Router.ts
@@ -2,10 +2,12 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 
 import WelcomePage from './components/WelcomePage.vue'
 import ArtistConfigurationPage from './components/ArtistConfigurationPage.vue'
+import Carousel from './components/Carousel.vue'
 
 const routes = [
     { path: '/', component: WelcomePage},
-    { path: '/configure', component: ArtistConfigurationPage}
+    { path: '/configure', component: ArtistConfigurationPage},
+    { path: '/carousel', component: Carousel}
 ]
 
 const router = createRouter({
