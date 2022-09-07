b0e7f90f055b38d02a4d0186bfd58cb1385e0ad6
Kasper <kwolsink@gmail.com>
2022-09-07 15:00:21 +0200

    basic carousel ui skeleton

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,3 +1,21 @@
 <template>
-    Hello Carousel!
+    <div class=" h-screen flex flex-col">
+        <div class="banner flex flex-row border-b-2 border-primary-color h-10 items-center justify-center pl-2 pr-2">
+            <span class="middle font-semibold text-primary-color">
+                Mix With Me
+            </span>
+        </div>
+        
+
+        <div class="footer mt-auto flex flex-row border-t-2 border-primary-color h-14 items-center justify-between">
+            <span>prof</span>
+            <span>car</span>
+            <span>match</span>
+        </div>
+    </div>
+
 </template>
+
+
+<style scoped>
+</style>
