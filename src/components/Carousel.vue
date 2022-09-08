7665a9614e909dc4b72ed060a71cd0f4dfa1a9f5
Kasper <kwolsink@gmail.com>
2022-09-08 09:39:32 +0200

    ui update

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class=" h-screen flex flex-col">
-        <div class="banner flex flex-row border-b-2 border-primary-color h-10 items-center justify-center pl-2 pr-2">
-            <span class="middle font-semibold text-primary-color">
-                Mix With Me
-            </span>
-        </div>
-        <ProfileUI v-if="userProfile !== undefined" :user-profile="userProfile"></ProfileUI>
+    <div class="banner fixed top-0 left-0 w-screen flex flex-row border-b-2 border-primary-color h-10 items-center justify-center pl-2 pr-2 bg-primary-color">
+        <span class="middle font-semibold text-white">
+            Mix With Me
+        </span>
+    </div>
 
-        <div class="footer mt-auto flex flex-row border-t-2 border-primary-color h-14 items-center justify-between">
+    <div class="flex flex-col h-screen justify-center">
+
+        <ProfileUI v-if="userProfile !== undefined" :user-profile="userProfile"></ProfileUI>
+    </div>
+
+    <div class="footer fixed bottom-0 left-0 w-screen mt-auto flex flex-row border-t-2 border-primary-color h-14 items-center justify-between">
             <span>prof</span>
             <span>car</span>
             <span>match</span>
-        </div>
     </div>
 
 </template>
 
 <script lang="ts" setup>
 import ProfileUI from './ProfileUI.vue';
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { onValue, ref as dbref } from '@firebase/database';
 import firebase from '../firebaseconfig';
 import store from '../store';
@@ -28,7 +30,7 @@ if (store.getCurrentUser() !== undefined) { // sync userProfile with the db valu
     const userProfileRef = dbref(firebase.database, `/users/${store.getCurrentUser()?.uid}/profile`)
     onValue(userProfileRef, (snapshot) => {
         userProfile.value = snapshot.val()
-    }, {onlyOnce: true})
+    }, { onlyOnce: true })
 }
 
 
--- src/components/ProfileUI.vue
+++ src/components/ProfileUI.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="flex flex-col p-5 h-screen justify-center">
-        <h1 class=" text-red-800 text-4xl font-semibold mb-2">
-            {{userProfile.artistName}}
-        </h1>
-        <div class="tags flex flex-row mb-2"> 
-            <Tag v-if="userProfile.skills.producer" tag-text="Producer" tag-style="tag-style-1"></Tag>
-            <Tag v-if="userProfile.skills.singer" tag-text="Singer" tag-style="tag-style-2"></Tag>
-            <Tag v-if="userProfile.skills.rapper" tag-text="Rapper" tag-style="tag-style-3"></Tag>
+    <div class="flex flex-col justify-center">
+        <div class="card">
+            <h1 class=" text-orange-400 text-4xl font-semibold mb-2">
+                {{userProfile.artistName}}
+            </h1>
+            <div class="tags flex flex-row mb-2">
+                <Tag v-if="userProfile.skills.producer" tag-text="Producer" tag-style="tag-style-1"></Tag>
+                <Tag v-if="userProfile.skills.singer" tag-text="Singer" tag-style="tag-style-2"></Tag>
+                <Tag v-if="userProfile.skills.rapper" tag-text="Rapper" tag-style="tag-style-3"></Tag>
+            </div>
+            <p class=" text-gray-500">
+                {{userProfile.bio.length === 0 ? 'This user has no biography' : userProfile.bio}}
+            </p>
         </div>
-        <p class=" text-gray-500 mb-2">
-            {{userProfile.bio.length === 0 ? 'This user has no biography' : userProfile.bio}}
-        </p>
-        <div v-if="userProfile.lookingFor.length > 0" class="looking-for">
-            <h3 class="text-red-800 text-2xl font-semibold">I am looking for:</h3>
-            <p class="text-gray-500 mb-2">
+        <div v-if="userProfile.lookingFor.length > 0" class="looking-for card">
+            <h3 class="text-orange-400 text-2xl font-semibold">I am looking for:</h3>
+            <p class="text-gray-500">
                 {{userProfile.lookingFor}}
             </p>
         </div>
-        <div v-if="userProfile.skills.producer" class="flex flex-col producer-tags mb-2">
-            <h3 class="text-red-800 text-2xl font-semibold">I use the following DAW(s):</h3>
-            <div class="tag-container flex flex-row">  
+        <div v-if="userProfile.skills.producer" class="flex flex-col producer-tags mb-2 card">
+            <h3 class="text-orange-400 text-2xl font-semibold">I use the following DAW(s):</h3>
+            <div class="tag-container flex flex-row">
                 <Tag v-if="userProfile.producerSkills.abletonLive" tag-text="Ableton"></Tag>
                 <Tag v-if="userProfile.producerSkills.flStudio" tag-text="FL Studio"></Tag>
                 <Tag v-if="userProfile.producerSkills.logicPro" tag-text="Logic Pro"></Tag>
@@ -43,3 +45,9 @@ defineProps({
 
 
 </script>
+
+<style scoped>
+.card {
+    @apply bg-gray-100 rounded-md shadow-gray-400 shadow-md m-3.5 p-5
+}
+</style>
